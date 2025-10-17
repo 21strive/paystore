b405f78002ccb1cf1ad64c3284752ea67f28bee9
Bernino Falya <falya.bernino@gmail.com>
2025-10-17 19:35:36 +0700

    init payment fetcher and fetch workflow

--- fetch_workflow.go
+++ fetch_workflow.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"github.com/redis/go-redis/v9"
+	"paystore/config"
+	"paystore/payment"
+)
+
+type PaystoreFetcher struct {
+	paymentFetcher payment.FetcherClient
+}
+
+func (pf *PaystoreFetcher) FetchByBalance(lastRandId []string, balanceUUID string) ([]*payment.Payment, *string, *string, bool, error) {
+	isBlank, err := pf.paymentFetcher.IsBlankByBalance(balanceUUID)
+	if err != nil {
+		return nil, nil, nil, false, err
+	}
+	if isBlank {
+		return nil, nil, nil, false, nil
+	}
+
+	payments, validLastRandId, position, errFetch := pf.paymentFetcher.FetchByBalance(lastRandId, balanceUUID)
+	if errFetch != nil {
+		return nil, nil, nil, false, errFetch
+	}
+	if int64(len(payments)) < pf.paymentFetcher.GetItemPerPage() {
+		return payments, nil, nil, true, nil
+	}
+
+	return payments, &validLastRandId, &position, false, nil
+}
+
+func NewFetcher(redis redis.UniversalClient, config *config.App) *PaystoreFetcher {
+	paymentFetcher := payment.NewFetcher(redis, config)
+	return &PaystoreFetcher{
+		paymentFetcher: paymentFetcher,
+	}
+}
--- payment/payment_fetcher.go
+++ payment/payment_fetcher.go
@@ -1,1 +1,44 @@
 package payment
+
+import (
+	"github.com/21strive/redifu"
+	"github.com/redis/go-redis/v9"
+	"paystore/config"
+)
+
+type FetcherClient interface {
+	FetchByBalance(lastRandId []string, balanceRandId string) ([]*Payment, string, string, error)
+	IsBlankByBalance(balanceRandId string) (bool, error)
+	GetItemPerPage() int64
+}
+
+type Fetcher struct {
+	base              *redifu.Base[*Payment]
+	timelineByBalance *redifu.Timeline[*Payment]
+}
+
+func (f *Fetcher) FetchByBalance(lastRandId []string, balanceRandId string) ([]*Payment, string, string, error) {
+	return f.timelineByBalance.Fetch([]string{balanceRandId}, lastRandId, nil, nil)
+}
+
+func (f *Fetcher) IsBlankByBalance(balanceRandId string) (bool, error) {
+	isBlank, errCheck := f.timelineByBalance.IsBlankPage([]string{balanceRandId})
+	if errCheck != nil {
+		return false, errCheck
+	}
+
+	return isBlank, nil
+}
+
+func (f *Fetcher) GetItemPerPage() int64 {
+	return f.timelineByBalance.GetItemPerPage()
+}
+
+func NewFetcher(redis redis.UniversalClient, config *config.App) *Fetcher {
+	base := redifu.NewBase[*Payment](redis, "payment:%s", config.RecordAge)
+	timelineByBalance := redifu.NewTimeline[*Payment](redis, base, "payment-balance:%s", config.ItemPerPage, redifu.Descending, config.PaginationAge)
+	return &Fetcher{
+		base:              base,
+		timelineByBalance: timelineByBalance,
+	}
+}
